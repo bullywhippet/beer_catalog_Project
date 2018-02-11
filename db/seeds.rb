fdf941454b2cbe9b39fb894c094ff0727b0fcb99
Kelly Walters <kellywdev@gmail.com>
2018-02-11 23:25:55 +0000

    create seed information for Style table from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+Style.destroy_all
+
+# add style info to db
+styles_csv = File.read(Rails.root.join('lib', 'seeds', 'styles.csv'))
+styles_csv_parsed = CSV.parse(styles_csv, headers: true)
+
+styles_csv_parsed.each do |row|
+  Style.create(name: row.to_hash['style_name'])
+  puts "Added value: #{row.to_hash['style_name']} to Style Table"
+end
+
