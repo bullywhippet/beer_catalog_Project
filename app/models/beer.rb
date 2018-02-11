ee69d6afc22c32363f72aa491ae7374cd0d25902
Kelly Walters <kellywdev@gmail.com>
2018-02-11 22:43:00 +0000

    beer belongs to style and brewery

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ApplicationRecord
+  belongs_to :style
+  belongs_to :brewery
   validates :name, :style_id, presence: true
   validates :abv, numericality: true
   validates :name, uniqueness: true
