4898ba6418e47a571e61c79b81067fea5dcaa544
Kelly Walters <kellywdev@gmail.com>
2018-02-12 02:39:42 +0000

    add bagination to breweries display

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,7 +4,7 @@ class BreweriesController < ApplicationController
   # GET /breweries
   # GET /breweries.json
   def index
-    @breweries = Brewery.all
+    @breweries = Brewery.page(params[:page]).per(10)
   end
 
   # GET /breweries/1
