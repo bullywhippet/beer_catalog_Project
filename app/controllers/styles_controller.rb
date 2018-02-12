1eecf98adc1937936d2b424c40d0fa2dabef5a75
Kelly Walters <kellywdev@gmail.com>
2018-02-12 02:39:02 +0000

    added find info to show methid for style controller

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -10,6 +10,7 @@ class StylesController < ApplicationController
   # GET /styles/1
   # GET /styles/1.json
   def show
+    @style = Style.find(params[:id])
   end
 
   # GET /styles/new
@@ -28,7 +29,7 @@ class StylesController < ApplicationController
 
     respond_to do |format|
       if @style.save
-        format.html { redirect_to @style, notice: 'Style was successfully created.' }
+        format.html { redirect_to @style, notice: 'Style was successfully created.'}
         format.json { render :show, status: :created, location: @style }
       else
         format.html { render :new }
@@ -62,13 +63,14 @@ class StylesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_style
-      @style = Style.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def style_params
-      params.require(:style).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_style
+    @style = Style.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def style_params
+    params.require(:style).permit(:name)
+  end
 end
