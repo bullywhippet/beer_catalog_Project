d427bd5fd28e73003319e3d73f28ae6a233308ac
Kelly Walters <kellywdev@gmail.com>
2018-02-13 18:14:39 +0000

    seed data now pulls in associations and populates beers

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,52 +8,52 @@
 require 'csv'
 
 Style.destroy_all
+Style.reset_pk_sequence
 Brewery.destroy_all
-# Beer.destroy_all
+Brewery.reset_pk_sequence
+Beer.destroy_all
+Beer.reset_pk_sequence
 
 # add style info to db
 styles_csv = File.read(Rails.root.join('lib', 'seeds', 'styles.csv'))
 styles_csv_parsed = CSV.parse(styles_csv, headers: true)
 
-styles_csv_parsed.each do |row|
-  style = Style.create(name: row.to_hash['style_name'])
-  puts "Added value: #{row.to_hash['style_name']} to Style Table"
-  if style.save
-    puts "Added value: #{row.to_hash['style_name']} to Style Table"
-  else
-    puts "Could not add value: #{row.to_hash['style_name']} to Style Table"
-  end
-end
-
-# add brewery info to db
 brewery_csv = File.read(Rails.root.join('lib', 'seeds', 'breweries.csv'))
 brewery_csv_parsed = CSV.parse(brewery_csv, headers: true)
-brewery_csv_parsed.each do |row|
-  brew = Brewery.new(name: row.to_hash['name'],
-                     address: row.to_hash['address1'],
-                     city: row.to_hash['city'],
-                     state: row.to_hash['state'],
-                     country: row.to_hash['country'])
-  if brew.save
-    puts "Added value: #{row.to_hash['name']} to Brewery Table"
-  else
-    puts "Could not add value: #{row.to_hash['name']} to Brewery Table"
-  end
+
+beers_csv = File.read(Rails.root.join('lib', 'seeds', 'beers.csv'))
+beers_csv_parsed = CSV.parse(beers_csv, headers: true)
+
+
+
+styles_csv_parsed.each do |row|
+  Style.create(name: row.to_hash['style_name'])
 end
+puts "created #{Style.count} records"
+
+#brewery
+brewery_csv_parsed.each do |item|
+  Brewery.create(name: item.to_hash['name'],
+                 address: item.to_hash['address1'],
+                 city: item.to_hash['city'],
+                 state: item.to_hash['state'],
+                 country: item.to_hash['country'])
+  puts "."
+
+end
+puts "created #{Brewery.count} records"
 
 
-# add beer info to db
-# beers_csv = File.read(Rails.root.join('lib', 'seeds', 'beers.csv'))
-# beers_csv_parsed = CSV.parse(beers_csv, headers: true)
-# beers_csv_parsed.each do |row|
-#   beer = Brewery.new(name: row.to_hash['name'],
-#                      description: row.to_hash['description'],
-#                      style_id: row.to_hash['style_id'],
-#                      abv: row.to_hash['abv'],
-#                      brewery_id: row.to_hash['brewery_id'])
-#   if beer.save
-#     puts "Added value: #{row.to_hash['name']} to Beer Table"
-#   else
-#     puts "Could not add value: #{row.to_hash['name']} to Beer Table"
-#   end
-# end
+beers_csv_parsed.each do |thing|
+  st_id = Style.find(thing.to_hash['style_id'])
+  brewery = Brewery.find(thing.to_hash['brewery_id'])
+  beer = brewery.beers.create(name: thing.to_hash['name'],
+                              description: thing.to_hash['description'],
+                              style: st_id,
+                              abv: thing.to_hash['abv'])
+  puts beer.name
+end
+puts "created #{Beer.count} records"
+puts Brewery.first.inspect
+puts Style.first.inspect
+puts Beer.first.inspect
