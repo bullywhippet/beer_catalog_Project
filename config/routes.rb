a9c73d867f20e4d3162e5814682680b9f7e51bce
Kelly Walters <kellywdev@gmail.com>
2018-02-14 03:09:34 +0000

    use faker data in seed file to fill in descriptions

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ Rails.application.routes.draw do
   resources :breweries
   resources :beers
   resources :styles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
+puts 'Removing existing data'
 Style.destroy_all
 Style.reset_pk_sequence
 Brewery.destroy_all
@@ -24,36 +25,54 @@ brewery_csv_parsed = CSV.parse(brewery_csv, headers: true)
 beers_csv = File.read(Rails.root.join('lib', 'seeds', 'beers.csv'))
 beers_csv_parsed = CSV.parse(beers_csv, headers: true)
 
+# Prints a text-based "spinner" element while work occurs.
+spinner = Enumerator.new do |e|
+  loop do
+    e.yield '|'
+    e.yield '/'
+    e.yield '-'
+    e.yield '\\'
+  end
+end
 
+count = styles_csv_parsed.length
 
+puts 'Creating Style records'
 styles_csv_parsed.each do |row|
   Style.create(name: row.to_hash['style_name'])
+  printf("\r  Creating Records: %s", spinner.next)
+  sleep(0.1)
 end
-puts "created #{Style.count} records"
+puts " >> Created #{Style.count} records"
 
 #brewery
+puts 'Creating Brewery Records'
 brewery_csv_parsed.each do |item|
   Brewery.create(name: item.to_hash['name'],
                  address: item.to_hash['address1'],
                  city: item.to_hash['city'],
                  state: item.to_hash['state'],
                  country: item.to_hash['country'])
-  puts "."
-
+  printf("\r  Creating records: %s", spinner.next)
 end
-puts "created #{Brewery.count} records"
+puts " >> Created #{Brewery.count} records"
 
 
 beers_csv_parsed.each do |thing|
-  st_id = Style.find(thing.to_hash['style_id'])
+
+  random_description = "Surprisingly contains #{Faker::Beer.malts}"
+
+  style_ref = Style.find(thing.to_hash['style_id'])
   brewery = Brewery.find(thing.to_hash['brewery_id'])
   beer = brewery.beers.create(name: thing.to_hash['name'],
-                              description: thing.to_hash['description'],
-                              style: st_id,
+                              description: random_description,
+                              style: style_ref,
                               abv: thing.to_hash['abv'])
-  puts beer.name
+
+  printf("\r  Creating records: %s", spinner.next)
 end
-puts "created #{Beer.count} records"
+puts " >> Created  #{Beer.count} records"
+
 puts Brewery.first.inspect
 puts Style.first.inspect
 puts Beer.first.inspect
